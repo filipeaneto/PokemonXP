c5fcefa9f29cc14b72eca5631a06d657b40b1252
Filipe Neto <filipeaneto@gmail.com>
2012-10-27 16:06:35 -0200

    Sprite reescrito

---
+++ lua/drawable.lua
@@ -0,0 +1,34 @@
+--[[
+   drawable.lua
+   This file is part of PokémonXP
+  
+   Copyright (C) 2012 - Filipe Neto
+  
+   PokémonXP is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+  
+   PokémonXP is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+  
+   You should have received a copy of the GNU General Public License
+   along with PokémonXP. If not, see <http://www.gnu.org/licenses/>.
+]]
+
+require "lua/type"
+
+Drawable = {}
+
+Type(Drawable, function(drawable, loveDrawable)
+    
+    drawable.drwImage = loveDrawable -- userdata
+
+end)
+
+-- utiliza os mesmos argumentos de love.graphics.draw
+function Drawable:draw(...)
+    love.graphics.draw(self.drwImage, ...)
+end
